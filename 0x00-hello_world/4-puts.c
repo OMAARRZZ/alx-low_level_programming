5b3c434a8a8847f1c8b9a4fdcd14a6c10343fffc
OMAARRZZ <omar662002@hotmail.com>
2023-09-07 07:33:44 -0700

    c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+/**
+ * main - Entry point
+ * The program prints the desired statement 
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
+
