8005bef1982a02afe518485e7679ab9094d12e70
OMAARRZZ <omar662002@hotmail.com>
2023-09-07 07:58:10 -0700

    printf file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+/* 
+ * The main functions of this program 
+ * printing the desired statement
+ * Return:Always (0) success
+ */
+#include<stdio.h>
+int main()
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
