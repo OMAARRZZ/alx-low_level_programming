51a562e31821b9c074eed6033d43b965a66f4b93
OMAARRZZ <omar662002@hotmail.com>
2023-09-07 07:35:46 -0700

    for betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 /**
  * main - Entry point
- * The program prints the desired statement 
+ * The program prints the desired statement
  * Return: Always 0 (Success)
  */
 #include <stdio.h>
 
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
-
