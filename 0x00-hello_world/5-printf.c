9377a740034597aee82a047f8a067f675807b2f5
OMAARRZZ <omar662002@hotmail.com>
2023-09-07 08:07:58 -0700

    meh

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 /*
+ * main - Entry point
  * The main functions of this program
  * printing the desired statement
  * Return:Always 0 (success)
  */
 #include<stdio.h>
-int main(void) /*main function*/
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
