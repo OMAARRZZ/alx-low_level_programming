c99a0d52243654829bfa8ab0f3308478bbd1fb99
OMAARRZZ <omar662002@hotmail.com>
2023-09-07 08:02:28 -0700

    c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-/* 
- * The main functions of this program 
+/*
+ * The main functions of this program
  * printing the desired statement
- * Return:Always (0) success
+ * Return:Always 0 (success)
  */
 #include<stdio.h>
-int main()
+int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,"\n);
 return (0);
 }
