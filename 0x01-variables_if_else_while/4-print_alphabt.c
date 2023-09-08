bc46d77eb003ce395840f8c1372cd0c51aee265f
OMAARRZZ <omar662002@hotmail.com>
2023-09-08 16:53:12 -0700

    hohoho

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * description: prints all letters but 2
+ * Return always 0 (successfull)
+ */
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+if (letter != 'q' && letter != 'e')
+{
+putchar(letter);
+}
+}
+putchar('\n');
+return 0;
+}
