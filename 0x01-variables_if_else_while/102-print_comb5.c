3a19485427d07632f4df7cb86fdd60553d90755b
OMAARRZZ <omar662002@hotmail.com>
2023-09-08 19:26:24 -0700

    finalee

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -4,16 +4,16 @@
  * description: prints all possible combinations of two two-digit numbers.
  * Return: always 0 (successful)
  */
-int main(void) 
+int main(void)
 {
 int i, z, j, y;
-for (i = 0; i < 10 ; i++)
+for (i = 0; i < 10; i++)
 {
-for (z = 0; z < 10 ; z++)
+for (z = 0; z < 10; z++)
 {
-for (j = 0; j < 10 ; j++)
+for (j = 0; j < 10; j++)
 {
-for (y = 0; y < 10 ; y++)
+for (y = 1; y < 10; y++)
 {
 putchar('0' + i);
 putchar('0' + z);
@@ -23,63 +23,6 @@ putchar('0' + y);
 if (i != 9 || z != 9 || j != 9 || y != 9)
 {
 putchar(',');
-putchar(' ');
-}
-}
-}
-}
-}
-putchar('\n');
-return (0);
-};
-for (i = 0;i < 10 ; i++)
-{
-for (z = 0;z < 10 ; z++)
-{
-for (j = 0;j < 10 ; j++)
-{
-for (y = 0;y < 10 ; y++)
-{   
-putchar('0' + i);
-putchar('0' + z);
-putchar(' ');
-putchar('0' + j);
-putchar('0' + y);
-if (i != 9 || z != 9 || j != 9 || y != 9 )
-{
-putchar(',');#include <stdio.h>
-/**
- * main - entry point
- * description: prints all possible combinations of two two-digit numbers.
- * Return: always 0 (successful)
- */
-int main(void) {
-int i, z, j, y;
-for (i = 0;i < 10 ; i++)
-{
-for (z = 0;z < 10 ; z++)
-{
-for (j = 0;j < 10 ; j++)
-{
-for (y = 0;y < 10 ; y++)
-{
-putchar('0' + i);
-putchar('0' + z);
-putchar(' ');
-putchar('0' + j);
-putchar('0' + y);
-if (i != 9 || z != 9 || j != 9 || y != 9 )
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
-}
-putchar('\n');
-return (0);
-}
 putchar(' ');
 }
 }
