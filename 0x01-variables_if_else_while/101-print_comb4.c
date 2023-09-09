08f89cba5c97c078ff13b1738b8501e715eb3d45
OMAARRZZ <omar662002@hotmail.com>
2023-09-08 18:28:16 -0700

    advanced

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * description: prints all numbers 3 diff digit numbers
+ * Return: always 0 (successful)
+ */
+int main(void) {
+int i, j, k;
+for (i = 0; i <= 9; i++) {
+for (j = i + 1; j <= 9; j++) {
+for (k = j + 1; k <= 9; k++) {
+putchar(i + '0');
+putchar(j + '0');
+putchar(k + '0');
+if (i != 7 || j != 8 || k != 9) {
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
