e46e38b37e78909e9dcffa00578b3501d948a018
OMAARRZZ <omar662002@hotmail.com>
2023-09-12 16:43:11 -0700

    lele

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * times_table - prints the 9 time table
+ * Return: void
+ */
+void times_table(void)
+{
+for (int i=0; i<10; i++)
+{
+printf("%d",i*9);
+}
+}
