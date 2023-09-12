fd0c29b74ece9c39f4e1bd3e161754324da82534
OMAARRZZ <omar662002@hotmail.com>
2023-09-12 15:16:20 -0700

    meme

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _abs - checks if n is +ve or -ve
+ * @c: is the value to be checked
+ * Return: absolute value
+ */
+int int _abs(int c)
+{
+int absvlue;
+absvlue=abs(c);
+return (absvlue);
+}
