d91fdc044030f2ae71b1d70c8e232b9e64626a88
OMAARRZZ <omar662002@hotmail.com>
2023-09-12 13:33:22 -0700

    alph

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<main.c>
 /**
  * main - entry point
  *
@@ -8,7 +9,8 @@
  */
 void print_alphabet(void)
 {
-for (char c = 'a'; c <= 'z'; c++)
+char c;
+for (c = 'a'; c <= 'z'; c++)
 _putchar(c);
 _putchar('\n');
 }
