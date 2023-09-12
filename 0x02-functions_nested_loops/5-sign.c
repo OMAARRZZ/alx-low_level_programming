c57950e2caeaf8ef5185602b609879767dc02f50
OMAARRZZ <omar662002@hotmail.com>
2023-09-12 14:57:36 -0700

    checks sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_sign - checks if n is +ve or -ve
+ * @n: is the value to be checked
+ * Return: 1 if +ve, 0 if zero, -1 if -ve
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+return (1);
+}
+else if (n == 0)
+{
+return (0);
+}
+else
+{
+return (-1);
+}
+}
