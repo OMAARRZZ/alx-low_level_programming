20360fc33a7db68bd5711cd0827c8cc95f09d52e
OMAARRZZ <omar662002@hotmail.com>
2023-09-12 13:21:05 -0700

    new

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * 
+ * Description: prints _putchar
+ * 
+ * Return: Always 0 (successfull)
+ */
+int main(void)
+{
+printf("_putchar\n");
+return 0;
+}
