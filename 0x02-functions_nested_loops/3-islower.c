49b5e51721f4622ab13b2bbe559e40af71f198e5
OMAARRZZ <omar662002@hotmail.com>
2023-09-12 14:33:07 -0700

    meme

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabets 10 times
+ */
+void int _islower(int c);
+{
+if (c <= 'a' && c >= 'z')
+{
+    return (1);
+}
+else 
+return (0);
+}
