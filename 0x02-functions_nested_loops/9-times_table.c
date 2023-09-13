c2301f5eec3297e619e6da3aa787180d5aa84b72
OMAARRZZ <omar662002@hotmail.com>
2023-09-12 17:38:53 -0700

    meme

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,11 +10,11 @@ int num, mult, prod;
 for (num = 0 ; num <= 9; num++)
 {
 _putchar(0);
-for (mult = 1; mult <= 9; mult++)
+for (mult =1; mult <=9; mult++)
 {
+prod = num * mult;
 _putchar(' ');
 _putchar(',');
-prod = num * mult;
 if (prod <= 9)
 {
 _putchar(' ');
