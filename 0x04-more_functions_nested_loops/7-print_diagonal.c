e98b1ce7374ea34590cad6236addcb7a4ffc58df
OMAARRZZ <omar662002@hotmail.com>
2023-09-14 13:13:48 -0700

    lele

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_diagonal - prints a diagonal line of backslashes in the terminal
+ * @n: The number of times the character '\' should be printed
+ */
+void print_diagonal(int n)
+{
+int i, spaces;
+if (n <= 0)
+{
+_putchar('\n');
+return;
+}
+for (i = 0; i < n; i++)
+{
+for (spaces = 0; spaces < i; spaces++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
