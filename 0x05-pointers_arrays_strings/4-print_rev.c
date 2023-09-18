57722457ea4763f9f3358540cd240eb06faf0785
OMAARRZZ <omar662002@hotmail.com>
2023-09-18 13:02:31 -0700

    lkele

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,9 +9,12 @@ void print_rev(char *s)
 int length = 0;
 while (s[length] != '\0')
 {
-length++;  
+length++;
 }
-for (int i = length - 1; i >= 0; i--)
-write(1, &s[i], 1);
+while (length > 0)
+{
+length--;
+write(1, &s[length], 1);
+}
 write(1, "\n", 1);
 }
