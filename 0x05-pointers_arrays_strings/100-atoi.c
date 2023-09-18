b88282b2a70729dec268915f9db4e89d90680d19
OMAARRZZ <omar662002@hotmail.com>
2023-09-18 14:16:15 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,18 +6,23 @@
  */
 int _atoi(char *s)
 {
-int result = 0;
-int sign = 1;
 int i = 0;
-while (s[i] == '-' || s[i] == '+')
+int sign = 1;
+int result = 0;
+while (s[i] != '\0')
 {
 if (s[i] == '-')
+{
 sign *= -1;
-i++;
 }
-while (s[i] >= '0' && s[i] <= '9')
+else if (s[i] >= '0' && s[i] <= '9')
 {
 result = result * 10 + (s[i] - '0');
+if (s[i + 1] < '0' || s[i + 1] > '9')
+{
+break;
+}
+}
 i++;
 }
 return (result * sign);
