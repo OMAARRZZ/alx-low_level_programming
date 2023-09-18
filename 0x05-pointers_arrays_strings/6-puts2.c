4be6c04da0c4d332db4afa74f0aaa8e6b1a10ca7
OMAARRZZ <omar662002@hotmail.com>
2023-09-18 13:39:36 -0700

    puts

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <unistd.h>
 /**
  * puts2 - Prints every other character of a string.
  * @str: Pointer to a string.
@@ -9,8 +8,11 @@ void puts2(char *str)
 int i = 0;
 while (str[i] != '\0')
 {
-write(1, &str[i], 1);
-i += 2;
+if (i % 2 == 0)
+{
+printf("%c", str[i]);
 }
-write(1, "\n", 1);
+i++;
+}
+printf("\n");
 }
