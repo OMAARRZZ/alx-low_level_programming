e0e4cab94addb77db9c8d21d1bde23029fab2b44
OMAARRZZ <omar662002@hotmail.com>
2023-09-21 03:27:13 -0700

    meme

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include"main.h"
+/**
+ * _strncpy - Copies at most n bytes from src to dest.
+ * @dest: Pointer to the destination string.
+ * @src: Pointer to the source string to be copied.
+ * @n: Maximum number of bytes to copy.
+ * Return: Pointer to the resulting string dest.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i = 0;
+while (i < n && src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+while (i < n)
+{
+dest[i] = '\0';
+i++;
+}
+return (dest);
+}
