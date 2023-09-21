eb2234e3aff2e9279dd5d4222217279549259bd5
OMAARRZZ <omar662002@hotmail.com>
2023-09-21 03:37:30 -0700

    toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * string_toupper - Converts all lowercase letters to uppercase.
+ * @str: Pointer to the input string.
+ * Return: Pointer to the resulting string.
+ */
+char *string_toupper(char *str)
+{
+char *ptr = str;
+while (*ptr != '\0')
+{
+if (*ptr >= 'a' && *ptr <= 'z')
+*ptr -= 32;
+ptr++;
+}
+return str;
+}
