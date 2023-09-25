03596c5bdd43dd2cd5447e47a747b71836653127
OMAARRZZ <omar662002@hotmail.com>
2023-09-25 11:18:56 -0700

    str

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_chessboard - prints the chessboard
+ * @a: 2D array representing the chessboard
+ */
+void print_chessboard(char (*a)[8])
+{
+int i = 0;
+while (i < 8)
+{
+int j = 0;
+while (j < 8)
+{
+putchar(a[i][j]);
+j++;
+}
+putchar('\n');
+i++;
+}
+}
