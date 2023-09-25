6b5e4976509e87845eff13bdd19f854eb39cf742
OMAARRZZ <omar662002@hotmail.com>
2023-09-25 11:11:30 -0700

    brk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,20 +9,20 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-    int i, j;
-    i = 0;
-    while (s[i])
-    {
-        j = 0;
-        while (accept[j])
-        {
-            if (s[i] == accept[j])
-            {
-                return (s + i);
-            }
-            j++;
-        }
-        i++;
-    }
-    return (NULL);
+int i, j;
+i = 0;
+while (s[i])
+{
+j = 0;
+while (accept[j])
+{
+if (s[i] == accept[j])
+{
+return (s + i);
+}
+j++;
+}
+i++;
+}
+return (NULL);
 }
