73b4997d6b86c879e176e0e82e079bcf7d1d7bdc
OMAARRZZ <omar662002@hotmail.com>
2023-09-28 16:43:51 -0700

    fixes and changes

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,31 +8,28 @@ int is_prime_number(int n)
 {
 if (n <= 1)
 {
-return 0;
+return (0);
 }
-else
-{
-return is_prime_recursive(n, 2);
-}
+return (check_prime(n, 2));
 }
 /**
- * is_prime_recursive - Recursive function to check for primality
+ * check_prime - Helper function to check for primality
  * @n: The number to check for primality
  * @divisor: The current divisor being tested
  * Return: 1 if n is prime, 0 otherwise
  */
-int is_prime_recursive(int n, int divisor)
+int check_prime(int n, int divisor)
 {
 if (divisor > n / 2)
 {
-return 1;
+return (1);
 }
 else if (n % divisor == 0)
 {
-return 0;
+return (0);
 }
 else
 {
-return is_prime_recursive(n, divisor + 1);
+return (check_prime(n, divisor + 1));
 }
 }
