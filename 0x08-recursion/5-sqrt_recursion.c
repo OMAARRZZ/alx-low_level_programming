1414ef1b424faf392a5d7aa8c638dab200cc0312
OMAARRZZ <omar662002@hotmail.com>
2023-09-28 16:34:44 -0700

    sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * helper - helper function to find square root
+ * @n: number to find square root of
+ * @i: possible square root
+ * Return: natural square root or -1 if does not exist
+ */
+int helper(int n, int i)
+{
+if (i * i > n)
+{
+return (-1);
+}
+else if (i * i == n)
+{
+return (i);
+}
+return (helper(n, i + 1));
+}
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to find square root of
+ * Return: natural square root or -1 if does not exist
+ */
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+{
+return (-1);
+} else
+{
+return (helper(n, 0));
+}
+}
