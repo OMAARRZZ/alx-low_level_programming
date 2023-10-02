836a349187c830759b8e1a6ee1b6986bec5276ff
OMAARRZZ <omar662002@hotmail.com>
2023-10-02 09:05:45 -0700

    lele

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments
+ * @argv: An array containing the command-line arguments
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+int i = 0;
+void(argv);
+while (i < argc)
+{
+printf("%s\n", argv[i]);
+i++;
+}
+return (0);
+}
