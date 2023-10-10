994a36a287944c9550c5605116c1125f8c5f839e
omaarrwael <omar662002@hotmail.com>
2023-10-10 15:22:18 -0700

    fixx

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,7 +6,10 @@
  */
 void print_dog(struct dog *d)
 {
-printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
-printf("Age: %.6f\n", d->age != NULL ? d->age : "(nil)");
-printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
+if (d != NULL)
+{
+printf("Name: %s\n", d->name ? d->name : "(nil)");
+printf("Age: %.6f\n", d->age);
+printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+}
 }
