8acbad3f828583ea6c37e950f88710f284feb622
omaarrwael <omar662002@hotmail.com>
2023-10-10 16:39:56 -0700

    meh

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,7 +10,7 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-if (name != NULL && age != NULL && owner != NULL)
+if (name != NULL && owner != NULL)
 {
 dog_t *newDog = malloc(sizeof(dog_t));
 if (newDog == NULL)
