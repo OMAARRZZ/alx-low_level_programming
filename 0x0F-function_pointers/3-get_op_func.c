fb0b1f12b1dcedc2834c829611eeb72b2a73c551
omaarrwael <omar662002@hotmail.com>
2023-10-11 14:42:28 -0700

    fixes

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -7,29 +7,23 @@
  * @s: The operator passed as an argument to the program
  * Return: A pointer to the function that corresponds to the operator given as a parameter.
 */
-int (get_op_func(chars))(int, int)
+int (*get_op_func(char *s))(int, int)
 {
 op_t ops[] = {
 {"+", op_add},
 {"-", op_sub},
-{"", op_mul},
+{"*", op_mul},
 {"/", op_div},
 {"%", op_mod},
 {NULL, NULL}
-};
+ };
 int i = 0;
 while (ops[i].op)
 {
-if ((ops[i].op) == s)
-{
+if (*(ops[i].op) == *s)
 return (ops[i].f);
-}
 i++;
 }
 printf("Error\n");
-if ((s + 1) == '\0')
-{
-return (NULL);
-}
 exit(99);
 }
