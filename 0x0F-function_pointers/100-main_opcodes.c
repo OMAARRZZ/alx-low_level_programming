850954dec3b12f48b31725d7247cb9c7794cb2c6
omaarrwael <omar662002@hotmail.com>
2023-10-11 13:58:18 -0700

    100

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+void print_opcodes(char *main_func_start, int num_bytes);
+int main(int argc, char *argv[])
+{
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+int num_bytes = atoi(argv[1]);
+if (num_bytes < 0)
+{
+printf("Error\n");
+return (2);
+}
+char *main_func_start = (char *)main;
+print_opcodes(main_func_start, num_bytes);
+return (0);
+}
+void print_opcodes(char *main_func_start, int num_bytes)
+{
+printf("Opcode for main:\n");
+for (int i = 0; i < num_bytes; i++)
+{
+printf("%02x", main_func_start[i] & 0xFF);
+if (i != num_bytes - 1)
+{
+printf(" ");
+}
+}
+printf("\n");
+}
