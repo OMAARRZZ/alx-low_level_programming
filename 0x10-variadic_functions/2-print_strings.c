92f870080d5ce88c30938fd618cf3692b21dd8dd
omaarrwael <omar662002@hotmail.com>
2023-10-12 15:48:07 -0700

    fix

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -7,13 +7,13 @@
  * @n: The number of strings passed to the function.
  * Return: void
  */
-void print_strings(const char *separator, const unsigned int n, ...) 
+void print_strings(const char *separator, const unsigned int n, ...)
 {
 va_list args;
 unsigned int i = 0;
 const char *current_str;
 va_start(args, n);
-while (i < n) 
+while (i < n)
 {
 current_str = va_arg(args, const char *);
 if (current_str == NULL)
