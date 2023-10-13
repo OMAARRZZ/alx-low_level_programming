a9199b435333dea179aaa2b951d1465ff5736793
omaarrwael <omar662002@hotmail.com>
2023-10-13 07:56:03 -0700

    fix

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,14 +2,6 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-void _putchar(char c);
-/**
  * print_all - function that prints anything.
  * @format: list of types of arguments passed to the function
  * Description: 
