36d8adb3ffde4f826ca15216ef280ad2040019fa
omaarrwael <omar662002@hotmail.com>
2023-10-20 04:39:23 -0700

    fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,26 +1,47 @@
-#include <stdio.h>
-#include "lists.h"
+#include"lists.h"
 /**
- * print_list - Prints all the elements of a list_t list.
- * @h: Pointer to the head of the list.
- * Return: The number of nodes in the list.
+ * print_list - Prints all the elements of a list_t list
+ * @h: A pointer to the head of the list
+ * Return: The number of nodes
  */
 size_t print_list(const list_t *h)
 {
-size_t count = 0;
-const list_t *current = h;
-while (current != NULL)
-{
-if (current->str == NULL)
-{
-printf("[0] (nil)\n");
-}
-else
-{
-printf("[%d] %s\n", current->len, current->str);
-}
-count++;
-current = current->next;
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			_putchar('[');
+			_putchar('0');
+			_putchar(']');
+			_putchar(' ');
+			_putchar('(');
+			_putchar('n');
+			_putchar('i');
+			_putchar('l');
+			_putchar(')');
+			_putchar('\n');
+		}
+		else
+		{
+			size_t len = h->len;
+			const char *str = h->str;
+			size_t i;
+			_putchar('[');
+			for (i = 0; i < len; i++)
+			{
+				_putchar(str[i]);
+			}
+			_putchar('[');
+			_putchar(' ');
+			_putchar('(');
+			_putchar('0');
+			_putchar(')');
+			_putchar('\n');
+		}
+		h = h->next;
+		count++;
 	}
-return (count);
+	return (count);
 }
