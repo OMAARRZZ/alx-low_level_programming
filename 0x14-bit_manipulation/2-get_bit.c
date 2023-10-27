15b932ff09e25e95f9a6ef4296dc13477ef30dfc
omaarrwael <omar662002@hotmail.com>
2023-10-27 13:30:57 -0700

    fixes

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,7 +10,7 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-unsigned long int mask; 
+unsigned long int mask;
 if (index >= sizeof(unsigned long int) * CHAR_BIT)
 {
 return (-1);
