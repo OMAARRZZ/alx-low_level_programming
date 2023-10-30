52d0ddfd1768a8d00d3c78b2b7f83943aa9b5bbf
omaarrwael <omar662002@hotmail.com>
2023-10-30 13:16:39 -0700

    fixes

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,46 +10,46 @@
  */
 int main(int argc, char *argv[])
 {
-int file_descriptor_from, file_descriptor_to, bytes_read, bytes_written;
+int fd_from, fd_to, bytes_read, bytes_written;
 char buffer[1024];
 if (argc != 3)
 {
 dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
 return (97);
 }
-file_descriptor_from = open(argv[1], O_RDONLY);
-if (file_descriptor_from == -1)
+fd_from = open(argv[1], O_RDONLY);
+if (fd_from == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 return (98);
 }
-file_descriptor_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-if (file_descriptor_to == -1)
+fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+if (fd_to == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 return (99);
 }
-while ((bytes_read = read(file_descriptor_from, buffer, sizeof(buffer))) > 0)
+while ((bytes_read = read(fd_from, buffer, sizeof(buffer))) > 0)
 {
-bytes_written = write(file_descriptor_to, buffer, bytes_read);
+bytes_written = write(fd_to, buffer, bytes_read);
 if (bytes_written == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-close(file_descriptor_from);
-close(file_descriptor_to);
+close(fd_from);
+close(fd_to);
 return (99);
 }
 }
 if (bytes_read == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-close(file_descriptor_from);
-close(file_descriptor_to);
+close(fd_from);
+close(fd_to);
 return (98);
 }
-if (close(file_descriptor_from) == -1 || close(file_descriptor_to) == -1)
+if (close(fd_from) == -1 || close(fd_to) == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't close file descriptor %d\n", (file_descriptor_from == -1) ? file_descriptor_to : file_descriptor_from);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", (fd_from == -1) ? fd_to : fd_from);
 return (100);
 }
 return (0);
