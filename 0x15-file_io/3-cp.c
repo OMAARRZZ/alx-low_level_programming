6c4440a181838719ac02dab78af8938251ce1c71
omaarrwael <omar662002@hotmail.com>
2023-10-30 13:24:05 -0700

    fixes

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -26,7 +26,7 @@ return (98);
 fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 if (fd_to == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 return (99);
 }
 while ((bytes_read = read(fd_from, buffer, sizeof(buffer))) > 0)
@@ -34,7 +34,7 @@ while ((bytes_read = read(fd_from, buffer, sizeof(buffer))) > 0)
 bytes_written = write(fd_to, buffer, bytes_read);
 if (bytes_written == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 close(fd_from);
 close(fd_to);
 return (99);
