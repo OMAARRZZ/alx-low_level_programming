5860028422f2a4e9f9dd0aebe66587a42e0f3526
omaarrwael <omar662002@hotmail.com>
2023-10-30 12:20:20 -0700

    fixes

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,11 +1,13 @@
 #include "main.h"
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standard output.
+ * read_textfile - Reads a text file and prints it 
+ * to the POSIX standard output.
  * @filename: The name of the file to read.
  * @letters: The number of letters to read and print.
  * Return: The actual number of letters read and printed. 0 on error.
  */
-ssize_t read_textfile(const char *filename, size_t letters) {
+ssize_t read_textfile(const char *filename, size_t letters)
+{
 int file_descriptor;
 ssize_t total_bytes = 0;
 char buffer[1024];
@@ -20,11 +22,11 @@ if (file_descriptor == -1)
 {
 return (0);
 }
-while (letters > 0) 
+while (letters > 0)
 {
 ssize_t read_size = (letters < sizeof(buffer)) ? letters : sizeof(buffer);
 bytes_read = read(file_descriptor, buffer, read_size);
-if (bytes_read == -1) 
+if (bytes_read == -1)
 {
 close(file_descriptor);
 return (0);
